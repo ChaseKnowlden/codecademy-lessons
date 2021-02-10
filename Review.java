2776b9cfff0d85308411a3fd1a715868ae596369
ChaseKnowlden <knowlden887@gmail.com>
2021-02-10 17:48:30 -0500

    Initial Review java

---
+++ Review.java
@@ -0,0 +1,12 @@
+public class Review {
+    public static void main(String[] args) {
+      // The main method executes the tasks of the class
+      System.out.println("My first Java program from scratch!");
+    }
+
+    int something = 0;
+    double price = 1.7;
+    boolean there = true;
+    char grade = 'B';
+    String yourName = "Eric";
+}
